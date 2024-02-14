a07968ee474c3f24ad24056b75d2f7d0f73952b7
emelecio966 <143520582+emelecio966@users.noreply.github.com>
2024-02-14 15:37:33 +0000

    Play rock

--- app.rb
+++ app.rb
@@ -13,13 +13,14 @@ get("/rock") do
 
   @comp_move = moves.sample 
 
- # if comp_move == "rock"
- #   @outcome = "tied"
- # elsif @comp_move == "paper"
- #   @outcome = "lost"
- # else 
- #   @outcome = "won"
- # end 
+  if @comp_move == "rock"
+   @outcome = "tied"
+  elsif @comp_move == "paper"
+   @outcome = "lost"
+  else 
+   @outcome = "won"
+  end 
 
   erb(:zebra)
 end 
+
