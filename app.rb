a4e8f417dc4dabd8f0c155452f54a6c8f0adf789
emelecio966 <143520582+emelecio966@users.noreply.github.com>
2024-02-15 15:39:26 +0000

    rules

--- app.rb
+++ app.rb
@@ -2,10 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Howdy!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  
 end
 
 get("/rock") do
@@ -24,3 +21,18 @@ get("/rock") do
   erb(:zebra)
 end 
 
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample 
+
+  if @comp_move == "paper"
+   @outcome = "tied"
+  elsif @comp_move == "rock"
+   @outcome = "lost"
+  else 
+   @outcome = "won"
+  end 
+
+  erb(:giraffe)
+end
